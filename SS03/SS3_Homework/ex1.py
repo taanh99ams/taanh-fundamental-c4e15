9bdd0d51617c4ce6be2b1c5f8368b884ad42cf7f
Ta Anh <taanh1999gha@gmail.com>
2018-01-19 11:10:31 +0700

    initial commit

--- SS03/SS3_Homework/ex1.py
+++ SS03/SS3_Homework/ex1.py
@@ -8,9 +8,8 @@ else:
     i = 2
     while i < num:
         if num % i == 0:
-            print(num, "is not prime number")
-            break
-        else:
-            print(num, "is prime number")
+            print(num, "is not a prime number")
             break
         i += 1
+    else:
+        print(num, "is a prime number")
