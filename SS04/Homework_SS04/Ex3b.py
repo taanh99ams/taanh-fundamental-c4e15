762b9401181e4ed5df1ad3627db3ec0b1e77fc40
Ta Anh <taanh1999gha@gmail.com>
2018-01-23 17:11:40 +0700

    initial commit

---
+++ SS04/Homework_SS04/Ex3b.py
@@ -0,0 +1,17 @@
+game = []
+x = ["book", "pencil", "board", "eraser", "chalk"]
+
+import random
+game.append(random.choice(x).lower())
+
+from random import shuffle
+y = [i for i in game[0]]
+shuffle(y)
+print(*y)
+
+answer = (input("Please input your guess for the word about: ")).lower()
+
+if answer == game[0].lower():
+    print ("Hura")
+else:
+    print (":(")
